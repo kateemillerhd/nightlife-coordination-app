b18e075a833ccc124b63d2fe9accfd18bc149ed0
kateemillerhd <kateemillerhd@gmail.com>
2025-06-30 02:51:57 +0000

    connected MongoDB

--- server/server.js
+++ server/server.js
@@ -3,10 +3,18 @@ const express = require('express');
 const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
 
 const apiKey = process.env.YELP_API_KEY;
 const mongoUri = process.env.MONGO_URI;
 const sessionSecret = process.env.SESSION_SECRET;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() = console.log('Connected to MongoDB'))
+.catch((err) => console.error('MongoDB connection error:', err));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
