0ad0d709020338d72903efb33c9e7c2dc89aa885
kateemillerhd <kateemillerhd@gmail.com>
2025-06-30 02:56:36 +0000

    fix: syntax error in server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() = console.log('Connected to MongoDB'))
+.then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
 app.use(express.json());
