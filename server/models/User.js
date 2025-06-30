370af2b749a0bad6dbd577ef8292aae1c4ec0a8b
kateemillerhd <kateemillerhd@gmail.com>
2025-06-30 21:26:56 +0000

    updated user.js to add user authentication

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,25 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
   attending: [String]
 });
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+userSchema.methods.comparePassword = function (candidate) {
+  return bcrypt.compare(candidate, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
