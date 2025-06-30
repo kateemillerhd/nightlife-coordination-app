43ad3de1bf5226a6e01e49bdcec8ab0315215a94
kateemillerhd <kateemillerhd@gmail.com>
2025-06-30 03:00:39 +0000

    Create /models/Bar.js

--- server/models/Bar.js
+++ server/models/Bar.js
@@ -1,1 +1,16 @@
-// mongo model
+const mongoose = require('mongoose');
+
+const barSchema = new mongoose.Schema({
+  yelpId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: String,
+  location: String,
+  image_url: String,
+  url: String,
+  attendees: [String]
+});
+
+module.exports = mongoose.model('Bar', barSchema);
